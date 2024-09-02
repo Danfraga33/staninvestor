dfb484e9c74558653e7f7b78af2d009a8edff2d4
Daniel Fraga <dan.fraga33@gmail.com>
2024-09-02 18:55:58 +0100

    feat: add sector dataframe

--- main.py
+++ main.py
@@ -4,18 +4,43 @@ from ta.utils import dropna
 import yfinance as yf
 import finnhub
 import json
+import os
+
 
 finnhub_client = finnhub.Client(api_key="co44i4hr01qqksebmuggco44i4hr01qqksebmuh0")
-us_symbols = finnhub_client.stock_symbols('US')
 
-tickers = []
-for stock in us_symbols: 
-   ticker = stock.get("displaySymbol", "Unknown")
-   tickers.append(ticker)
-   
-print(tickers)
+us_sectors = [  
+    'XLC',  
+    'XLY',  
+    'XLP',  
+    'XLE',  
+    'XLB',  
+    'XLF', 
+    'XLV' 
+    'XLI',
+    'XLRE',
+    "XLK", 
+    'XLU'] 
 
-df = yf.download("^SPX", start="2023-01-01")
+
+
+
+            
+# US STOCKS
+arcx_data = finnhub_client.stock_symbols('US', "ARCX")
+df_arcx = pd.DataFrame(arcx_data)
+us_sector_indicies = df_arcx[df_arcx["symbol"].isin(us_sectors)]
+df_sectors = pd.DataFrame(us_sector_indicies)
+df_sectors.to_csv('us_sectors.csv', index=False)
+
+# NASDAQ TICKERS
+nasdaq_symbols = finnhub_client.stock_symbols('US', "XNAS")
+df_tickers = pd.DataFrame(nasdaq_symbols)
+df_tickers.to_csv('tickers.csv', index=False)
+
+tickers = pd.read_csv('tickers.csv')['symbol'].tolist()
+sectors = pd.read_csv("us_sectors.csv")
+
 
 def calculate_relative_strength(stock_prices, benchmark_prices, window=50):
    df = pd.DataFrame({
