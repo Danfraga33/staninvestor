a68879d78dd43f668b735fbde579b0349ba31e6e
Daniel Fraga <dan.fraga33@gmail.com>
2024-09-07 21:19:56 +0100

    feat: add sector map

---
+++ sector.py
@@ -0,0 +1,12 @@
+gics_to_spdr = {
+    'Consumer Cyclical': 'XLC', 
+    'Consumer Staples': 'XLP',  
+    'Energy': 'XLE',  
+    'Financials': 'XLF',  
+    'Healthcare': 'XLV',  
+    'Industrials': 'XLI',  
+    'Technology': 'XLK',  
+    'Utilities': 'XLU',  
+    'Real Estate': 'XLRE',  
+    'Communication Services': 'XLC',
+    'Materials': 'XLB'  }
