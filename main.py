e3b1b6eace6417c80f0c4ab176322aca2065e05f
Daniel Fraga <dan.fraga33@gmail.com>
2024-09-11 21:25:10 +0100

    feat: add flask to backend

--- main.py
+++ main.py
@@ -6,9 +6,14 @@ import finnhub
 import plotly.express as px
 import requests
 import os
-
+from datetime import datetime
+from sector import gics_to_spdr
+from indicators import calculate_relative_strength, thirty_week_MA
+from flask import Flask,jsonify
+from flask_cors import CORS
 
 finnhub_client = finnhub.Client(api_key="co44i4hr01qqksebmuggco44i4hr01qqksebmuh0")
+pd.set_option('display.max_rows', None)
 
 us_sectors = [  
     'XLC',  
@@ -24,8 +29,9 @@ us_sectors = [
     'XLU'] 
 
 
-
-
+app = Flask(__name__)
+CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
             
 # US SECTORS
@@ -34,83 +40,130 @@ df_arcx = pd.DataFrame(arcx_data)
 us_sector_indicies = df_arcx[df_arcx["symbol"].isin(us_sectors)]
 df_sectors = pd.DataFrame(us_sector_indicies)
 df_sectors.to_csv('us_sectors.csv', index=False)
+# SPDR SECTORS
+sector_data = pd.read_excel("sectors.xlsx")
+
 
 # NASDAQ 100 TICKERS
 nasdaq_100_symbols = pd.read_excel("nasdaq_100_stocks.xlsx")
 df_nasdaq_100 = pd.DataFrame(nasdaq_100_symbols)
-
-# NASDAQ SECTOR MAPPING (GSIC to SPDR)
-gics_to_spdr = {
-    'Consumer Cyclical': 'XLC', 
-    'Consumer Staples': 'XLP',  
-    'Energy': 'XLE',  
-    'Financials': 'XLF',  
-    'Healthcare': 'XLV',  
-    'Industrials': 'XLI',  
-    'Technology': 'XLK',  
-    'Utilities': 'XLU',  
-    'Real Estate': 'XLRE',  
-    'Communication Services': 'XLC',
-    'Materials': 'XLB'  }
 
 # CLEANING NASDAQ DATA
 df_nasdaq_100["Sector"] = df_nasdaq_100["Sector"].map(gics_to_spdr)
 df_nasdaq_100_cleaned = df_nasdaq_100.dropna(subset=['Sector'])
 df_nasdaq_100_cleaned.to_csv("nasdaq100.csv")
 nasdaq_tickers = pd.read_csv("nasdaq100.csv")
-
-# SPDR SECTORS
-sector_data = pd.read_excel("sectors.xlsx")
-
-
-output_dir = "stocks"
-if not os.path.exists(output_dir):
-    os.makedirs(output_dir)
     
+#  Comment out as it runs and slows down each code run. 
 # for ticker in nasdaq_tickers["Ticker"]:
 #    try:
 #       data = yf.download(ticker, "2015-01-01", interval="5d")
-#       file_path = os.path.join(output_dir, f"{ticker}.csv")
-   
+#       file_path = os.path.join(output_dir, f"{ticker}.csv")   
 #       data.to_csv(file_path)
 #    except Exception as e:
 #       print(f"Could not download data for {ticker}: {e}")
-      
-
-data = yf.download("XLF", "2015-01-01", interval="5d")
-# fig = px.line(data, x = data.index, y=data["Adj Close"], title="ads")
-# st.title("Name")
-# st.plotly_chart(fig)
-
-stock_data = pd.read_csv('stocks/ZM.csv')
-# print(stock_data)
-sector_data = data[["Close"]].reset_index()
-print(data)
 
 
+# Constants
+default_start_date = "2022-01-01"
+window = 30
+volume_window = 12
+spx_ticker = "^GSPC"     
+ndx_ticker = "^NDX"
+# Chart
+selected_stock = st.sidebar.selectbox("Stocks", placeholder="Choose a stock", options=nasdaq_tickers["Ticker"])
+st.title("Stan Weinstein Stock Analysis")
+start_date = st.sidebar.date_input("Start Date", value=datetime.strptime(default_start_date, "%Y-%m-%d"))
+ma, rs, volume = st.tabs(["MA", "RS", "Volume"])
+# Data
+stock_price_data = yf.download(selected_stock, start_date, interval="5d")
+ma_data = stock_price_data.rolling(window).mean()
+market_price_data = yf.download(spx_ticker, start_date, interval="5d")  
+nasdaq_price_data = yf.download(ndx_ticker, start_date, interval="5d")  
+selected_stock_volume = stock_price_data["Volume"]
+volume_ma = selected_stock_volume.rolling(volume_window).mean() 
+df_volume = pd.DataFrame({
+    "Volume": selected_stock_volume,
+    "Volume Moving Average": volume_ma
+})
 
-def calculate_relative_strength(stock_prices, benchmark_prices, window=50):
-   df = pd.DataFrame({
-      "Stock":stock_prices,
-      "Benchmark":benchmark_prices
-   })
+stock = nasdaq_tickers[nasdaq_tickers["Ticker"] == selected_stock]
+stock_sector = stock["Sector"].values[0]
+sector_price_data = yf.download(stock_sector, start_date, interval="5d")  
+market_relative_trend = calculate_relative_strength(stock_price_data["Close"].values,market_price_data["Close"])
+nasdaq_relative_trend = calculate_relative_strength(stock_price_data["Close"].values,nasdaq_price_data["Close"])
+sector_relative_trend = calculate_relative_strength(stock_price_data["Close"].values,sector_price_data["Close"])
 
-   df["RS"] = df["Stock"]/df["Benchmark"]
-   
-   df["RS_MA"] = df["RS"].rolling(window=window).mean()
-   
-   return df[["RS", "RS_MA"]]
+checkbox = {
+    "Moving Average": False,
+    "Relative Strength": False,
+    "Volume Trend": False,
+} 
 
-# result = calculate_relative_strength(stock_data,sector_data)
-# print(result)
-# calculate_relative_strength("stocks/zm")
-# def thirty_week_MA(stock_prices, window=30):
-#    df = pd.DataFrame({
-#       "Stock_Prices":stock_prices
-#    })
-#    df["ThirtyW_MA"] = df["Stock_Prices"].rolling(window=window).mean()
-   
-#    return df[["ThirtyW_MA"]]
+with ma:
+    moving_average_indicator_description = "Price must close above the 30-week SMA and the SMA must be rising"
+    df_price_MA = pd.DataFrame({
+      "Date":stock_price_data.index,
+      "Stock Price": stock_price_data["Close"],
+      "Moving Average": ma_data["Close"]
+    })
+    if df_price_MA["Moving Average"][-1] > sum(df_price_MA["Moving Average"][-3:])/len(df_price_MA["Moving Average"][-3:]):
+        checkbox["Moving Average"] = True
+        st.markdown("Positive")
+        st.markdown(checkbox)
+    else:
+        st.markdown("No")
+         
+    # print(df_price_MA["Moving Average"][-1] > sum(df_price_MA["Moving Average"][-3:])/len(df_price_MA["Moving Average"][-3:]))
+    figMA = px.line(df_price_MA, x = stock_price_data.index, y=["Stock Price",'Moving Average'], color_discrete_map={
+        "Stock Price": "blue", "Moving Average":'orange'} , title=selected_stock)
+    st.markdown(moving_average_indicator_description)
+    st.plotly_chart(figMA)
+    
+with rs:
+    # TEXT
+    relative_strength_indicator_defintion = "The relative strength line tells you how strong a stock is relative to every other stock on the market"
+    guide_step1 = "1. The RS Line is rising."
+    guide_step2 = "2. The RS line is rising, showing outperformance."
+    guide_step3 = "3. The RS line confirms the price breakout, indicating strength relative to the market."
+    # DATA
+    if(market_relative_trend["RS_MA"][-1] > sum(market_relative_trend["RS_MA"][-3:])/len(market_relative_trend["RS_MA"][-3:])):
+        st.markdown("Yes")
+        checkbox["Relative Strength"] = True
+        st.markdown(checkbox)
+    fig_SPX = px.line(stock_price_data, x=stock_price_data.index, y=market_relative_trend["RS_MA"], title=f"{selected_stock} vs SPX")
+    fig_NDX = px.line(stock_price_data, x=stock_price_data.index, y=nasdaq_relative_trend["RS_MA"], title=f"{selected_stock} vs NDX")
+    fig_sector = px.line(stock_price_data, x = stock_price_data.index, y=sector_relative_trend["RS_MA"], title=f"{selected_stock} vs Sector: {stock_sector}")
+    st.markdown(relative_strength_indicator_defintion)
+    st.markdown(guide_step1)
+    st.markdown(guide_step2)
+    st.markdown(guide_step3)
+    
+    st.plotly_chart(fig_SPX)
+    st.plotly_chart(fig_NDX)
+    st.plotly_chart(fig_sector)
+
+with volume:
+    # print(df_volume["Volume"])
+    # print(df_volume["Volume"][-1])
+    if df_volume["Volume"][-1] > 2* (sum(df_volume["Volume Moving Average"][-3:])/len(df_volume["Volume Moving Average"][-4:])):
+        checkbox["Volume Trend"] = True
+        st.markdown("Positive")
+    
+    fig_volume = px.line(df_volume ,x=selected_stock_volume.index, y=["Volume", "Volume Moving Average"], color_discrete_map={
+        "Volume": "gray", "Volume Moving Average": "red"}, title=f"{selected_stock} Volume && MA")
+    st.plotly_chart(fig_volume)
 
 
 
+@app.route("/data")
+def get_data():
+    df = pd.DataFrame(stock_price_data["Close"])
+    result = [{date.strftime('%Y-%m-%d'): close} for date, close in df['Close'].items()]
+    print(result)
+    
+    return jsonify({"stockData": result})
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
